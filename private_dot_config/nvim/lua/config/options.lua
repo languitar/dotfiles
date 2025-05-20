2f570cc5f8134f781b92d87b6881ad282b637151
Johannes Wienke <languitar@semipol.de>
2025-05-20 12:56:41 +0200

    Initial nvim config

---
+++ private_dot_config/nvim/lua/config/options.lua
@@ -0,0 +1,9 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+
+-- I don't like this. ciw always kills my previously copied values from the system keyboard.
+vim.opt.clipboard = ""
+
+-- Don't auto-format by default
+vim.g.autoformat = false
