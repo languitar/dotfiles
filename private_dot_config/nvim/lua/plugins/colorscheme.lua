dad1ca36c0dac0ae2f052e75310c518fb6099995
Johannes Wienke <languitar@semipol.de>
2025-09-22 08:55:51 +0200

    Fix missing undercurl on windows wezterm WSL

--- private_dot_config/nvim/lua/plugins/colorscheme.lua
+++ private_dot_config/nvim/lua/plugins/colorscheme.lua
@@ -8,6 +8,14 @@ return {
           on_colors = function(c)
             c.border = c.blue0
           end,
+          on_highlights = function(hl, c)
+            if vim.fn.has("wsl") == 1 then
+              hl.SpellBad = { sp = c.error, undercurl = false, underline = true }
+              hl.SpellCap = { sp = c.warning, undercurl = false, underline = true }
+              hl.SpellLocal = { sp = c.info, undercurl = false, underline = true }
+              hl.SpellRare  = { sp = c.hint, undercurl = false, underline = true }
+            end
+          end
         })
         return tokyonight.load()
       end,
