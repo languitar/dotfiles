dbb4616e108ddd27c2cdd72391cb38ca3283502a
Johannes Wienke <languitar@semipol.de>
2025-10-29 09:54:23 +0100

    Disable snacks picker closing on escape
    Happens accidentally too often.

--- private_dot_config/nvim/lua/plugins/snacks.lua
+++ private_dot_config/nvim/lua/plugins/snacks.lua
@@ -3,16 +3,33 @@ return {
     "folke/snacks.nvim",
     opts = {
       terminal = {
-        shell = "fish"
+        shell = "fish",
       },
       picker = {
         formatters = {
           file = {
-            truncate = 120
-          }
-        }
-      }
+            truncate = 120,
+          },
+        },
+        -- Disable escape in picker to prevent it from accidentally closing
+        -- https://github.com/folke/snacks.nvim/discussions/1312
+        sources = {
+          explorer = {
+            win = {
+              input = {
+                keys = {
+                  ["<esc>"] = { "", mode = "n" },
+                },
+              },
+              list = {
+                keys = {
+                  ["<esc>"] = { "", mode = "n" },
+                },
+              },
+            },
+          },
+        },
+      },
     },
   },
 }
-
