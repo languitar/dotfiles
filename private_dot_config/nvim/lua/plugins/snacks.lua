bebaca58d78d49dfde4853e762daa515eebd6c59
Johannes Wienke <jwienke@schueco.com>
2026-06-22 13:30:22 +0200

    Disable linting in snacks notifications previews

--- private_dot_config/nvim/lua/plugins/snacks.lua
+++ private_dot_config/nvim/lua/plugins/snacks.lua
@@ -14,6 +14,14 @@ return {
         -- Disable escape in picker to prevent it from accidentally closing
         -- https://github.com/folke/snacks.nvim/discussions/1312
         sources = {
+          notifications = {
+            transform = function(item)
+              if item.preview then
+                item.preview.ft = nil
+              end
+              return item
+            end,
+          },
           explorer = {
             win = {
               input = {
